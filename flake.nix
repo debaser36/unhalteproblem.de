8c498d070af1d51b4de9f5209db3a398b502ac77
debaser36 <n.burkholder@tu-berlin.de>
2025-03-15 17:06:43 +0100

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,26 @@
 {
-  description = "Flake for unhalteproblem.de website and services";
+  description = Flake for unhalteproblem.de website and services;
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
 
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    src = ./.;
-    pnpmDeps = pkgs.pnpm.fetchDeps {
-      pname = "unhalteproblem-website";
-      version = "0.0.1";
-      src = src;
-      hash = "sha256-95op+Lebn95TDlihUs6kTdl2rSXEEOy47Qnmsa8bgHk="; # Auto-update this!
+  outputs = { self, nixpkgs }: 
+    let 
+      supportedSystems = [ x86_64-linux aarch64-linux x86_64-darwin ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSystems (system:
+        let 
+          pkgs = import nixpkgs { inherit system; };
+          src = ./.;
+          pnpmDeps = pkgs.pnpm.fetchDeps {
+            pname = unhalteproblem-website;
+            version = 0.0.1;
+            src = src;
+            hash = sha256-95op+Lebn95TDlihUs6kTdl2rSXEEOy47Qnmsa8bgHk=; # Auto-update this!
+          };
+        in {
+          default = pkgs.callPackage ./services/web/default.nix { inherit pnpmDeps; };
+        }
+      );
     };
-  in {
-    packages.x86_64-linux.default = pkgs.callPackage ./services/web/default.nix { inherit pnpmDeps; };
-    packages.x86_64-darwin.default = pkgs.callPackage ./services/web/default.nix { inherit pnpmDeps; };
-		packages.aarch64-linux.default = pkgs.callPackage ./services/web/default.nix { inherit pnpmDeps; };
-  };
 }
