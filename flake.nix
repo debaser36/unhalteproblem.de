e65cee320840d5458c71dbbd50fe83a71f0be39e
Nico <n.burkholder@tu-berlin.de>
2025-03-15 10:53:17 +0100

    initial setup

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "unhalteproblem.de Website for http/https";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.aarch64-linux.unhalteproblem.de = let
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+    in pkgs.stdenv.mkDerivation {
+      pname = "unhalteproblem.de";
+      version = "1.0";
+
+      src = ./src;  # Path to your website source code
+
+      buildInputs = [ pkgs.nodejs ];  # Adjust depending on what your website needs
+
+      buildPhase = ''
+        npm install
+        npm run build  # Adjust this command to your build process
+      '';
+
+      installPhase = ''
+        mkdir -p $out
+        cp -r dist/* $out/ 
+      '';
+    };
+  };
+}
+
